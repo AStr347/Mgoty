2b425577663ef2cba1e770ff61d8519cc08d4cf4
svistov <anton3000str@mail.ru>
2021-05-20 11:15:42 +0300

    fix(fiveth): int* -> vector<int>

--- fiveth.cpp
+++ fiveth.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 /**
@@ -9,9 +9,11 @@ using namespace std;
  * (a1 + bn)(a2 + bn–1)…(an + b1). 
 */
 
-void read_array(double* a, const int n){
+static void read_vector(vector<int>& v, const int n){
     for (int i = 0; i < n; i++){
-        cin >> a[i];
+        int _x = 0;
+        cin >> _x;
+        v.push_back(_x);
     }
 }
 
@@ -19,10 +21,10 @@ void fiveth_1(void){
     int n = 0;
     cin >> n;
     if(n != 0){
-        double* a = new double[n];
-        double* b = new double[n];
-        read_array(a,n);
-        read_array(b,n);
+        vector<int> a;
+        vector<int> b;
+        read_vector(a,n);
+        read_vector(b,n);
 
         long double result = a[0] + b[n-1];
         for (int i = 1; i < n; i++){
